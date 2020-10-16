3dc99c354f678aaacf567e31f49552e4d93f693e
Josuel Santos <josuel.santos@aluno.ifsp.edu.br>
2020-10-15 23:08:15 -0300

    Update 2020-10-09 - Atividade OrdRec.cpp
    A função agora descreve quais elementos estão sendo comparados e substituídos, melhorando a exibição.

--- 2020-10-09 - Atividade OrdRec.cpp
+++ 2020-10-09 - Atividade OrdRec.cpp
@@ -1,24 +1,38 @@
 #include <iostream>
 using namespace std;
 
-void insertionSort(int *v, int tam) {
-   int key, j;
-   for(int i = 1; i<tam; i++)
-   {
-      key = v[i];
-      j = i;
-      while(j > 0 && v[j-1]>key) {
-         v[j] = v[j-1];
-         j--;
-      }
-      v[j] = key;
-   }
+void insertion_sort(int t, int *a)
+{
+	for(int i=1;i<t;++i)
+	{
+		cout<<"Comparacao "<<i<<":"<<endl;
+		cout << "[" << i-1 << "]= " << a[i-1] << " com [" << i << "]= " << a[i]<<endl;
+		
+		int y,x;
+		y=i;
+		while(y>0&&a[y]<a[y-1])
+		{
+			x=a[y];
+			a[y]=a[y-1];
+			a[y-1]=x;
+			cout<<"Trocou e ficou assim: "<<endl;
+			cout << "[" << y-1 << "]= " << a[y-1] << " e [" << y << "]= " << a[y]<<endl;
+			y--;
+			if(y>0)
+			{
+			cout<<"Comparando "<<"[" << y-1 << "]= " << a[y-1] << " com [" << y << "]= " << a[y]<<endl;			
+			}
+		}
+		
+		cout<<"Comparacao "<<i<<" finalizada."<<endl<<endl;
+	}
+	cout << "------------------------" << endl;
 }
-   
+
 void print(int t, int *a) {
 	int i;
 	for(i=0; i<t; ++i) {
-		cout << "Elemento: " << i << " = " << a[i] << endl;
+		cout << "Elemento " << i+1 << " = " << a[i] << endl;
 	}
 	cout << "------------------------" << endl;
 }
@@ -28,9 +42,11 @@ int main(int argc, char** argv)
 	int v[8] = {26,49,38,13,58,87,34,93};
 	int tam = sizeof(v)/sizeof(int);
 	
-	print(tam,v);
-	insertionSort(v,tam);
-	print(tam,v);
+	print(tam , v);
+	
+	insertion_sort(tam , v);
+	
+	print(tam, v);
 	
 	return 0;
 }
